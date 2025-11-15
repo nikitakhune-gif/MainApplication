5c73e7a567d3e59a8e1549e9388f3fbec33d6087
nikitakhune-gif <nikitakhune163@gmail.com>
2025-11-15 20:47:17 +0530

    Add files via upload

---
+++ HolidayController.cs
@@ -0,0 +1,38 @@
+﻿using EmployeesLeaveApplication.Interface;
+using EmployeesLeaveApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesLeaveApplication.Controllers
+{
+    public class HolidayController : Controller
+    {
+        private readonly IHolidayRepository _repository;
+
+        public HolidayController(IHolidayRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var holidays = await _repository.GetAllAsync();
+            return View(holidays);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Holiday model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.AddAsync(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ LeaveController.cs
@@ -0,0 +1,44 @@
+﻿using EmployeesLeaveApplication.Models;
+using EmployeesLeaveApplication.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesLeaveApplication.Controllers
+{
+    public class LeaveController : Controller
+    {
+        private readonly ILeaveService _service;
+
+        public LeaveController(ILeaveService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var leaves = await _service.GetLeavesAsync();
+            return View(leaves);
+        }
+
+        public IActionResult ApplyLeave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ApplyLeave(LeaveApplication model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.AddAsync(model);
+                return RedirectToAction("LeaveHistory", new { employeeId = model.EmployeeId });
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> LeaveHistory(int employeeId)
+        {
+            var list = await _service.GetLeavesByEmployeeAsync(employeeId);
+            return View(list);
+        }
+    }
+}
